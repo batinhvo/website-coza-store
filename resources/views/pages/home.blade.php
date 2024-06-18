d933af33af32f7cf4cb83d117c9b5df4eafc6c17
VoBaTinh <batinhvo@gmail.com>
2024-06-19 00:04:39 +0700

    06192024

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -371,31 +371,20 @@
             </h3>
         </div>
 
-        <div class="flex-w flex-sb-m p-b-52">
+        <div class="flex-w flex-sb-m p-b-30">
             <div class="flex-w flex-l-m filter-tope-group m-tb-10">
-                <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5 how-active1" data-filter="*">
-                    All Products
-                </button>
-                @foreach($cate_show as $cate)
-                <button class="stext-106 cl6 hov1 bor3 trans-04 m-r-32 m-tb-5" data-filter=".{{$cate->cate_name}}">
-                {{$cate->cate_name}}
-                </button>
-                @endforeach
+                Best-selling product
             </div>
 
             <div class="flex-w flex-c-m m-tb-10">
-                <div class="flex-c-m stext-106 cl6 size-104 bor4 pointer hov-btn3 trans-04 m-r-8 m-tb-4 js-show-filter">
-                    <i class="icon-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-filter-list"></i>
-                    <i class="icon-close-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-close dis-none"></i>
-                        Filter
-                </div>
-
-                <div class="flex-c-m stext-106 cl6 size-105 bor4 pointer hov-btn3 trans-04 m-tb-4 js-show-search">
-                    <i class="icon-search cl2 m-r-6 fs-15 trans-04 zmdi zmdi-search"></i>
-                    <i class="icon-close-search cl2 m-r-6 fs-15 trans-04 zmdi zmdi-close dis-none"></i>
-                    Search
-                </div>
+                <a class="flex-c-m stext-106 cl6 size-105 bor4 pointer hov-btn3 trans-04 m-tb-4 btn-reload" onclick="reloadPage()">
+                    <i class="icon-search cl2 m-r-6 fs-15 trans-04 zmdi zmdi-refresh"></i>
+                    Refresh
+                </a>
             </div>
+            <script>
+                function reloadPage() { location.reload(); }
+            </script>
             
             <!-- Search product -->
             <div class="dis-none panel-search w-full p-t-10 p-b-15">
