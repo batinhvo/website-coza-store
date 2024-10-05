12d562d7cc14b84f8c606cc221e77ad31aed5ac1
VoBaTinh <batinhvo@gmail.com>
2024-10-05 16:17:49 +0700

    20241005

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\homeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [homeController::class, 'index']);
+Route::get('/home', [homeController::class, 'index']);
